8baef80c6c3ea3fafca02875cbdb0600b561c8e2
myholivia <myholivia@gmail.com>
2017-03-07 00:16:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myholivia/myminilinkedin/util/ImageUtils.java
@@ -0,0 +1,25 @@
+package com.example.myholivia.myminilinkedin.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class ImageUtils {
+
+    public static void loadImage(@NonNull Context context,
+                                 @NonNull Uri uri,
+                                 @NonNull ImageView imageView) {
+        try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
+            imageView.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
